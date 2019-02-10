f3d5a2d069ac6573fc876b8fd5f7b1e584e715c4
Marta Matos <marta.ra.matos@gmail.com>
2019-02-10 15:05:12 +0100

    Delete config_neer.js

--- lesson3/node_mysql_connection/config_neer.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'pijama',
-  database: process.env.DB_NAME || 'hyf'
-}
-
-module.exports = config;
